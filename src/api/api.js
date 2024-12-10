e70006dc0a7ad9b20bef4230dcbab03ef8ebb970
Maxyakubobskiy <115578152+Maxyakubobskiy@users.noreply.github.com>
2024-12-10 13:58:42 +0200

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api' // Ваш базовий URL бекенду
+  baseURL: 'https://carrentalb-production.up.railway.app/api' // Ваш базовий URL бекенду
 });
 
 api.interceptors.request.use((confirg)=>{
